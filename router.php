d3bff8cdfc541f534660f12f1e5677ca07727d37
okumurakengo <okumurakengo1a@gmail.com>
2017-11-23 19:48:19 +0900

    router.php update

--- router.php
+++ router.php
@@ -2,12 +2,10 @@
 // ルーティング設定
 if(preg_match('/\.css$/i',$_SERVER["REQUEST_URI"])) {
     header('Content-Type: text/css; charset=utf-8');
-    require __DIR__.$_SERVER["REQUEST_URI"];
+    require __DIR__.'/css/'.basename($_SERVER['PHP_SELF']);
 } elseif(preg_match('/\.js$/i',$_SERVER["REQUEST_URI"])){
     header('Content-Type: text/javascript; charset=utf-8');
-    require __DIR__.$_SERVER["REQUEST_URI"];
-} elseif(file_exists(__DIR__.$_SERVER["REQUEST_URI"]) && is_file(__DIR__.$_SERVER["REQUEST_URI"])){
-    require __DIR__.$_SERVER["REQUEST_URI"];
+    require __DIR__.'/js/'.basename($_SERVER['PHP_SELF']);
 } else {
     require __DIR__."/public/index.php";
 }
